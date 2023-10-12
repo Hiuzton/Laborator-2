b26b978feed6bde1a0a0c6c39c5fb31cdaad298c
Hiuzton <102302338+Hiuzton@users.noreply.github.com>
2023-10-12 14:52:18 +0300

    Add files via upload

--- gamma.js
+++ gamma.js
@@ -1,4 +1,9 @@
-window.addEventListener("deviceorientation", on_device_orientation);
+var culoareInchisa = "#6A0888";
+var culoareDeschisa = "#FF5733";
+
+var culoareCurenta = culoareInchisa;
+
+window.addEventListener("deviceorientation", on_device_orientation, false);
 
 function on_device_orientation(evt) {
   var gamma = evt.gamma;
@@ -9,8 +14,15 @@ function on_device_orientation(evt) {
   var centru = { x: canvas.width / 2, y: canvas.height / 2 };
   var latura_patrat = 50;
   var lungime_latura = latura_patrat + gamma;
+
+  if (gamma > 10) {
+    culoareCurenta = culoareDeschisa;
+  } else if (gamma < -10) {
+    culoareCurenta = culoareInchisa;
+  }
+
   context.clearRect(0, 0, canvas.width, canvas.height);
-  context.fillStyle = "#6A0888";
+  context.fillStyle = culoareCurenta;
   context.beginPath();
   context.fillRect(
     centru.x - lungime_latura / 2,
