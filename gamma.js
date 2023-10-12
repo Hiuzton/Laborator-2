15918cd33033a3b159e7d45d51d166c54b7cd521
Hiuzton <102302338+Hiuzton@users.noreply.github.com>
2023-10-12 14:40:25 +0300

    change to support ios

--- gamma.js
+++ gamma.js
@@ -1,4 +1,9 @@
-window.addEventListener("deviceorientation", on_device_orientation);
+if (window.DeviceOrientationEvent) {
+  window.addEventListener("deviceorientation", on_device_orientation, false);
+} else {
+  document.getElementById("c").innerHTML =
+    "Senzorii de orientare nu sunt suportați.";
+}
 
 function on_device_orientation(evt) {
   var gamma = evt.gamma;
@@ -8,7 +13,7 @@ function on_device_orientation(evt) {
   var context = canvas.getContext("2d");
   var centru = { x: canvas.width / 2, y: canvas.height / 2 };
   var latura_patrat = 50;
-  var lungime_latura = latura_patrat + gamma;
+  var lungime_latura = latura_patrat + gamma; // Modificăm dimensiunea în funcție de valoarea lui Gamma
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.fillStyle = "#6A0888";
   context.beginPath();
